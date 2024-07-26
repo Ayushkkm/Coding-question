43806893001cbed1d9ad078b98e1d64d668f6d80
shyam <ayushmbd21102002@gmail.com>
2024-07-26 22:02:56 +0530

    Sliding Window question

---
+++ Leetcode/Sliding-Window-and-Two-Pointers/Hard/5-Longest-Substring-with-Atmost-K-Distinct-Character.java
@@ -0,0 +1,90 @@
+
+// Better Solution
+
+class Solution {
+    public int longestkSubstr(String s, int k) {
+        
+        int n = s.length();
+        
+        int l = 0;
+        int r = 0;
+        
+        int maxL = -1;
+        
+        Map<Character , Integer> map = new HashMap<>();
+        
+        while(r < n){
+            
+            map.put(s.charAt(r) , map.getOrDefault(s.charAt(r), 0) + 1);
+            
+            while(map.size()  > k){ // shrink
+                
+                map.put(s.charAt(l) , map.get(s.charAt(l)) -1);
+                
+                if(map.get(s.charAt(l)) == 0){
+                    map.remove(s.charAt(l));
+                    
+                }
+                
+                l++;
+            }
+            
+            if(map.size() <= k){
+                
+                maxL = Math.max(maxL , r - l + 1);
+            }
+            
+            r++;
+        }
+        
+        return maxL;
+    }
+}
+
+// TC -> O(2N) + O(256)
+// SC -> O(256)
+
+// Optimal Solution
+
+class Solution {
+    public int longestkSubstr(String s, int k) {
+        
+        int n = s.length();
+        
+        int l = 0;
+        int r = 0;
+        
+        int maxL = -1;
+        
+        Map<Character , Integer> map = new HashMap<>();
+        
+        while(r < n){
+            
+            map.put(s.charAt(r) , map.getOrDefault(s.charAt(r), 0) + 1);
+            
+            if(map.size()  > k){ // shrink
+                
+                map.put(s.charAt(l) , map.get(s.charAt(l)) -1);
+                
+                if(map.get(s.charAt(l)) == 0){
+                    map.remove(s.charAt(l));
+                    
+                }
+                
+                l++;
+            }
+            
+            if(map.size() <= k){
+                
+                maxL = Math.max(maxL , r - l + 1);
+            }
+            
+            r++;
+        }
+        
+        return maxL;
+    }
+}
+
+// TC -> O(N) + O(256)
+// SC -> O(256)
