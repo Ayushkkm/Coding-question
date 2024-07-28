33afba3c4a1b3efe719a19b71a3f9707ff52c158
shyam <ayushmbd21102002@gmail.com>
2024-07-29 00:57:41 +0530

    Stack question

---
+++ Leetcode/Stack-and-Queue/2-Anuj-Bhaiya/11-Maximal-Rectangle.java
@@ -0,0 +1,103 @@
+class Solution {
+    
+    public int[] previousSmaller(int arr[]){
+        
+        int n = arr.length ;
+        
+        Stack<Integer> st = new Stack<>();
+
+        int ans[] = new int[n];
+
+        for(int i = 0 ;  i < n ; i++){
+
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = st.peek();
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    public int[] nextSmaller(int arr[]){
+        
+        int n = arr.length ;
+        
+        Stack<Integer> st = new Stack<>();
+
+        int ans[] = new int[n];
+
+        for(int i = n-1 ;  i >=0 ; i--){
+
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans[i] = n ; // instead -> of -1 -> push n
+            }
+            else{
+                ans[i] = st.peek();
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    public int maxHistogram(int[] heights) {
+        
+        int ps[] = previousSmaller(heights);
+        int ns[] = nextSmaller(heights);
+
+        int maxArea = 0;
+
+        for(int i = 0 ; i < heights.length ; i++){
+             
+             maxArea = Math.max(maxArea , (ns[i] - ps[i] - 1) * heights[i]);
+        }
+
+        return maxArea;
+    }
+
+    public int maximalRectangle(int arr[][]){
+
+        int currRow[] = arr[0];
+
+        int maxArea = maxHistogram(currRow);
+
+        for(int i = 1 ; i < arr.length ; i++){ // row0 -> already -> calculated -> go from 1
+
+            for(int j = 0 ; j < arr[0].length ; j++){
+
+                if(arr[i][j]==1){ // if 1 -> add to currRow
+                    currRow[j] += 1;
+                }
+
+                else {  // make that index 0
+                    currRow[j] = 0;
+                }
+
+            }
+
+            int currAns = maxHistogram(currRow); // find new -> area
+
+            maxArea = Math.max(maxArea , currAns);
+            
+        }
+
+        return maxArea;
+    }
+}
+
+// TC -> O(N*M)
+// SC -> O(N) -> stack space
