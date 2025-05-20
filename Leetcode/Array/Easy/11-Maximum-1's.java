11ecdb717baf32053f834b8b89409d29d2190942
shyam <ayushmbd21102002@gmail.com>
2025-05-20 23:12:10 +0530

    leetcode

--- Leetcode/Array/Easy/11-Maximum-1's.java
+++ Leetcode/Array/Easy/11-Maximum-1's.java
@@ -16,3 +16,48 @@ class Solution {
         return max;
     }
 }
+
+// Maximum Sum Array - 2 Solution
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+        
+//         int n = nums.length;
+
+//         int sum = 0;
+
+//         int maxSum = Integer.MIN_VALUE;
+
+//         for(int i = 0 ; i < n ; i++){
+
+//             sum += nums[i];
+
+//             maxSum = Math.max(maxSum , sum);
+
+//             if(sum<0){
+//                 sum = 0 ;
+//             }
+//         }
+
+
+//         return maxSum;
+//     }
+// }
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+    
+//     int currSum = 0;
+//     int maxSum = nums[0];
+
+//     for(int i = 0 ; i<nums.length ; i++){
+//        if(currSum<0) currSum = 0;
+
+//        currSum = currSum + nums[i];
+//        maxSum = Math.max(maxSum,currSum);
+//      }
+
+//     return maxSum;
+
+//   }
+// }
