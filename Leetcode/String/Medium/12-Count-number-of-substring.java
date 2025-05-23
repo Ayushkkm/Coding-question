dae0fd67c6b2aef6cc1ca7b71e52ff9e0ee44555
Ayushkkm <138689557+Ayushkkm@users.noreply.github.com>
2025-05-23 12:07:10 +0530

    Update 12-Count-number-of-substring.java

--- Leetcode/String/Medium/12-Count-number-of-substring.java
+++ Leetcode/String/Medium/12-Count-number-of-substring.java
@@ -1,68 +1,45 @@
 // link - https://www.geeksforgeeks.org/problems/count-number-of-substrings4528/1
 
-class Solution
-{
-    long substrCount (String S, int K) {
-       return atmost(S,K) - atmost(S,K-1); // Exactly K  
+class Solution {
+    int countSubstr(String S, int K) {
+        return atmost(S, K) - atmost(S, K - 1); // Exactly K  
     }
-    
-    long atmost(String s , int k){
-        
+
+    int atmost(String s, int k) {
         int dist = 0;
-        long ans = 0;
+        int ans = 0;
         int left = 0;
-        
+
         int hash[] = new int[26];
-        
-        for(int i = 0 ; i< s.length() ; i++){
-            hash[s.charAt(i)-'a']++; 
-            
-            if(hash[s.charAt(i)-'a']==1) dist++; // if 1 -> distinct -> if 2 not distint
-            
-            while(dist>k){
-                hash[s.charAt(left)-'a']--;  // left -> initial 0 -> character remove
-                
-                if(hash[s.charAt(left)-'a']==0) dist--;       // count -> left = 0 -> not in substring
-                left++; // left increase -> for next character
+
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i) - 'a']++;
+
+            if (hash[s.charAt(i) - 'a'] == 1) dist++; // if 1 -> distinct -> if 2 not distint
+
+            while (dist > k) {
+                hash[s.charAt(left) - 'a']--; // left -> initial 0 -> character remove
+
+                if (hash[s.charAt(left) - 'a'] == 0) dist--; // count -> left = 0 -> not in substring
+
+                left++; // left increase -> remove current character -> from string
             }
-            
-            ans = ans+ i-left+1; // rigth - left +1 -> give substring -> (left,left+1, ..., i) -> atmost k
-            
+
+            ans += i - left + 1;
+
+            /* 
+            If exactly K → then do: count++ → here atmost K 
+            Here → i - left + 1 → signifies number of substrings ending at i and starting from any index in [left, i]
+            Example: s = "abc", left = 0, i = 2 
+            Substrings: s[0..2] = "abc", s[1..2] = "bc", s[2..2] = "c"
+            So count += i - left + 1 → counts all substrings with at most K distinct characters.
+            while(dist > k) helps maintain the invariant of at most K distinct characters in the window.
+            */
         }
-        
+
         return ans;
     }
 }
 
-// Solution -> n^2 
-
-// class Solution
-// {
-//     long substrCount (String S, int K) {
-       
-//       int n = S.length();
-//       int ans = 0;
-//       int dist_ans = 0;
-       
-//       int hash[] = new int[26];
-       
-       
-//       for(int i = 0 ; i<n ; i++){
-//           dist_ans = 0;
-//           Arrays.fill(hash,0);
-           
-//           for(int j = i ; j<n ; j++){
-               
-//               if(hash[S.charAt(j)-'a']==0){
-//                   dist_ans++;
-//               }
-//               hash[S.charAt(j)-'a']++;
-               
-//               if(dist_ans==K) ans++;
-               
-//           }
-//       }
-       
-//       return ans ;
-//     }
-// }
+// TC -> O(N)
+// SC -> O(1)
