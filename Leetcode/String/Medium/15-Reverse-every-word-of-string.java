af82132158ae34bfe8d394c0c0c50985be096042
shyam <ayushmbd21102002@gmail.com>
2025-05-26 23:26:14 +0530

    leetcode

---
+++ Leetcode/String/Medium/15-Reverse-every-word-of-string.java
@@ -0,0 +1,69 @@
+class Solution {
+    public String reverseWords(String s) {
+        // Trim -> remove leading and trailing space , split -> remove space in between 2, 3 
+        // how many space it remove and split in array
+        
+        String[] words = s.trim().split("\\s+");
+        
+        
+        StringBuilder reversed = new StringBuilder();
+        
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i > 0) {
+                reversed.append(" ");
+            }
+        }
+        
+        return reversed.toString();
+    }
+}
+
+// Expression Meaning
+// \\s	-> Any whitespace character (space, tab, newline)
+// +	-> One or more occurrences
+// \\s+	-> One or more whitespace characters
+// .split("\\s+") -> Split string by one or more whitespace characters
+
+// Method 2 (easy)  
+
+// class Solution {
+//     public String reverseWords(String s) {
+//         int n = s.length();
+//         StringBuilder str = new StringBuilder();
+//         String ans = "";
+        
+//         for(int i = n-1 ; i>=0 ; i--){
+//             char c = s.charAt(i);
+            
+//             if(c == ' ' && str.length() == 0){ // to remove unnecessary spaces -> leading -> trailing -> middle
+//               continue;
+//             }
+
+//             if(c != ' '){
+//                 str.append(c);
+//             }
+//             else{
+//                 str.reverse();
+//                 if(ans.length() == 0){ // first -> without space
+//                     ans += str + "";
+//                 }
+//                 else{
+//                     ans += " " + str; // make space betwwen words
+//                 }
+//                 str = new StringBuilder(); // empty str -> now if a unnecessary spaces -> occur -> we continue it
+//             }
+//         }
+
+//         if(str.length() != 0){
+//             str.reverse();
+//             if(ans.length() == 0){ // first -> without space -> "aYhandf"
+//                 ans += str + "";
+//             }
+//             else{
+//                 ans += " " + str; // make space betwwen words
+//             }
+//         }
+//         return ans;
+//     }
+// }
