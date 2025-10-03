eeb5dfb7795ee76c8ff3aec8d04355359fba8641
Ayushkkm <138689557+Ayushkkm@users.noreply.github.com>
2025-10-03 16:20:44 +0530

    Update 15-2-Sum-Problem.java

--- Leetcode/Array/median/15-2-Sum-Problem.java
+++ Leetcode/Array/median/15-2-Sum-Problem.java
@@ -1,29 +1,51 @@
+
 class Solution {
-
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer , Integer> map = new HashMap<>();
-
-        map.put(nums[0], 0); // put here -> or in for loop 
-
-        int ans[] = new int[2];
-
-        for(int i = 1 ; i < nums.length ; i++){
-            int diff = target - nums[i];
-
-            if(map.containsKey(diff)){
-                ans[0] = map.get(diff);
-                ans[1] = i;
-
-                break;
-            }
-            else{
-                map.put(nums[i], i);
-            }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i< nums.length; i++){
+          int val = target - nums[i];
+          
+          if(map.containsKey(val)){
+            int j = map.get(val);
+            return new int[]{i,j};
+          }
+          
+          map.put(nums[i], i);
         }
-
-        return ans;
+        
+        return new int[]{};
+        
     }
 }
+
+// class Solution {
+
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer , Integer> map = new HashMap<>();
+
+//         map.put(nums[0], 0); // put here -> or in for loop 
+
+//         int ans[] = new int[2];
+
+//         for(int i = 1 ; i < nums.length ; i++){
+//             int diff = target - nums[i];
+
+//             if(map.containsKey(diff)){
+//                 ans[0] = map.get(diff);
+//                 ans[1] = i;
+
+//                 break;
+//             }
+//             else{
+//                 map.put(nums[i], i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
 
 // TC -> O(N)
 // SC -> O(N)
