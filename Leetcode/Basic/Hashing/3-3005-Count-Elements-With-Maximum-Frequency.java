a9bc53711a372b941471b94084872596d25fc5ae
Ayushkkm <138689557+Ayushkkm@users.noreply.github.com>
2025-10-08 16:51:27 +0530

    Update 3-3005-Count-Elements-With-Maximum-Frequency.java

--- Leetcode/Basic/Hashing/3-3005-Count-Elements-With-Maximum-Frequency.java
+++ Leetcode/Basic/Hashing/3-3005-Count-Elements-With-Maximum-Frequency.java
@@ -1,4 +1,3 @@
-
 class Solution {
     public int maxFrequencyElements(int[] nums) {
         int n = nums.length; 
@@ -34,3 +33,103 @@ class Solution {
         return count;
     }
 }
+
+// 5.5% with this -> map.constainsKey() -> maxHeap.contains() -> both return boolean
+
+// class pair implements Comparable<pair>{
+    
+//     int val;
+//     int freq;
+    
+//     pair(int val , int freq){
+      
+//        this.val = val;
+//        this.freq = freq;
+//     }
+    
+//     public int compareTo(pair other){
+//       return Integer.compare(other.freq , this.freq);
+//     }
+    
+// }
+
+
+// class Solution {
+ 
+//     public int maxFrequencyElements(int[] nums) {
+         
+//         Map<Integer, Integer> map = new HashMap<>();
+        
+//         for(int num : nums){
+//            map.put(num , map.getOrDefault(num , 0)+1);
+//         }
+        
+//         PriorityQueue<pair> maxHeap = new PriorityQueue<>();
+        
+//         for(int key : map.keySet()){
+//            maxHeap.offer(new pair(key , map.get(key)));
+//         }
+        
+//         int ans = 0;
+//         int max = 0;
+        
+//         while(!maxHeap.isEmpty()){
+//             pair p = maxHeap.poll();
+            
+//             max = Math.max(max, p.freq);
+            
+//             if(max != p.freq) break;
+            
+//             ans += p.freq;
+//         }
+        
+//         return ans ;
+//     }
+// }
+
+// 66% with hash and priority Queue 
+
+// class Solution {
+ 
+//     public int maxFrequencyElements(int[] nums) {
+         
+//           int max = nums[0];
+          
+//           for(int num : nums){
+//             max = Math.max(max , num);
+//           }
+          
+//           int hash[] = new int[max+1];
+          
+//           for(int num : nums){
+             
+//               hash[num]++;
+//           }
+          
+//           PriorityQueue<pair> maxHeap = new PriorityQueue<>();
+          
+//           for(int i = 0 ; i < max+1 ; i++){
+              
+//                if(hash[i] > 0){
+//                   maxHeap.offer(new pair(i , hash[i]));
+//                }
+//           }
+          
+//           int maxV = 0;
+//           int ans = 0;
+          
+//           while(!maxHeap.isEmpty()){
+              
+//               pair p = maxHeap.poll();
+              
+//               maxV = Math.max(maxV , p.freq);
+              
+//               if(maxV != p.freq) break;
+              
+//               ans += p.freq;
+              
+//           }
+          
+//           return ans ;
+//    }
+// }
